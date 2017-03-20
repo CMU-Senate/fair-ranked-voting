5a7592316cee249ea28696c321eed526d3e830c1
Devin Gund <devin@dgund.com>
2017-03-19 22:30:21 -0400

    Standardized docstrings for run.py interface

--- run.py
+++ run.py
@@ -20,44 +20,82 @@ import argparse
 import csv
 import re
 
+# String representing the input for No Confidence
 NC_STRING = "No Confidence"
+
+# String representing the abbreviated input for No Confidence
 NC_STRING_SHORT = "NC"
 
-def _input_string_is_no_confidence(input_string):
-    return (input_string.lower() == NC_STRING.lower
-            or input_string.lower() == NC_STRING_SHORT.lower())
+def input_string_is_no_confidence(candidate_input):
+    """Checks if an input string represents No Confidence.
 
-def _candidate_from_input(input_string):
-    if _input_string_is_no_confidence(input_string):
+    Args:
+        candidate_input: String representing user input for a Candidate. The
+            expected format is 'uid' or optionally 'uid (name)'.
+
+    Returns:
+        Boolean indicating if the input string represents No Confidence or not.
+    """
+    return (candidate_input.lower() == NC_STRING.lower
+            or candidate_input.lower() == NC_STRING_SHORT.lower())
+
+def candidate_from_input(candidate_input):
+    """Returns a Candidate representing the input string.
+
+    Args:
+        candidate_input: String representing user input for a Candidate. The
+            expected format is 'uid' or optionally 'uid (name)'.
+
+    Returns:
+        Candidate representing the input uid (and optionally name).
+    """
+    if input_string_is_no_confidence(candidate_input):
         return NoConfidence()
     else:
         expr = '(.*?)\s*\((.*?)\)'
         regex = re.compile(expr)
-        result = regex.match(input_string)
+        result = regex.match(candidate_input)
         if result != None:
             uid = result.group(1)
             name = result.group(2)
         else:
-            uid = input_string
+            uid = candidate_input
             name = None
         return Candidate(uid, name=name)
 
-def _ballot_from_candidate_inputs(candidate_inputs):
+def ballot_from_candidate_inputs(candidate_inputs):
+    """Returns a Ballot of Candidates representing the input strings.
+
+    Args:
+        candidate_inputs: List of Strings representing user input for a
+            Candidate. The expected format is 'uid' or optionally 'uid (name)'.
+
+    Returns:
+        Ballot representing the input Candidates.
+    """
     candidates = list()
     for candidate_input in candidate_inputs:
-        candidate = _candidate_from_input(candidate_input)
+        candidate = candidate_from_input(candidate_input)
         candidates.append(candidate)
     ballot = Ballot(candidates=candidates)
     return ballot
 
 def ballots_from_input():
+    """Return Ballots from command-line user input.
+
+    Returns:
+        List of Candidates representing user input.
+    """
     ballots = list()
     ballot_number = 0
 
     def print_ballot_instructions():
+        """"Prints ballot input instructions."""
+        print('Instructions:')
         print('Enter candidates on the ballot in a comma-separated-list of unique identifiers, ordered from most preferred to least preferred.')
+        print('Names may be optionally specified with the format \'uid (name)\'.')
         print('{} may also be abbreviated as {}.'.format(NC_STRING, NC_STRING_SHORT))
-        print('Press enter in an empty ballot to end input. Type undo to remove the last ballot.')
+        print('Press enter with an empty ballot to end input. Type \'undo\' to remove the last ballot. Type \'help\' to view these instructions again.')
 
     print_ballot_instructions()
     while True:
@@ -75,11 +113,19 @@ def ballots_from_input():
 
         else:
             candidate_inputs = [candidate_input.strip() for candidate_input in ballot_input.split(',')]
-            ballot = _ballot_from_candidate_inputs(candidate_inputs)
+            ballot = ballot_from_candidate_inputs(candidate_inputs)
             ballots.append(ballot)
             ballot_number += 1
 
 def ballots_from_csv(filename):
+    """Return Ballots from CSV user input.
+
+    Args:
+        filename: The filepath of the CSV file containing the user input.
+
+    Returns:
+        List of Ballots representing user input.
+    """
     ballots = list()
     with open(filename) as f:
         sniffer = csv.Sniffer()
@@ -90,23 +136,39 @@ def ballots_from_csv(filename):
         reader = csv.reader(f, dialect)
         for row in reader:
             if reader.line_num > 0 or not has_header:
-                ballot = _ballot_from_candidate_inputs(row)
+                ballot = ballot_from_candidate_inputs(row)
                 ballots.append(ballot)
     f.close()
     return ballots
 
 def ballots_from_txt(filename):
+    """Return Ballots from TXT user input.
+
+    Args:
+        filename: The filepath of the TXT file containing the user input.
+
+    Returns:
+        List of Ballots representing user input.
+    """
     ballots = list()
     with open(filename) as f:
         for line in f:
             if len(line.strip()) > 0:
                 candidate_inputs = [candidate_input.strip() for candidate_input in line.split(',')]
-                ballot = _ballot_from_candidate_inputs(candidate_inputs)
+                ballot = ballot_from_candidate_inputs(candidate_inputs)
                 ballots.append(ballot)
     f.close()
     return ballots
 
 def ballots_from_file(filename):
+    """Return Ballots from file user input.
+
+    Args:
+        filename: The filepath of the CSV or TXT file containing the user input.
+
+    Returns:
+        List of Ballots representing user input.
+    """
     if filename.lower().endswith('.csv'):
         return ballots_from_csv(filename)
     elif filename.lower().endswith('.txt'):
@@ -114,7 +176,45 @@ def ballots_from_file(filename):
     else:
         raise ValueError('Invalid filetype. Accepts .csv, .txt.')
 
-def main(args):
+def parse_args():
+    """Parses command-line election arguments.
+
+    Returns:
+        argparse.Namespace containing election arguments.
+    """
+    parser = argparse.ArgumentParser(description='Configure and run an election.')
+    required_group = parser.add_argument_group('required arguments')
+    
+    # Number of seats (required)
+    required_group.add_argument('-s','--seats', help='Number of seats', type=int, required=True)
+
+    # Alphanumeric string for breaking ties
+    parser.add_argument('-a','--alphanumeric', help='Alphanumeric string for breaking ties')
+    
+    # File containing ballots
+    parser.add_argument('-b','--ballots', help='File containing ballots')
+    
+    # Disallow No Confidence from being eliminated
+    parser.add_argument('-c','--disallow-nc-elimination', help='No Confidence cannot be eliminated', action='store_true')
+    
+    # Name of Election
+    parser.add_argument('-n','--name', help='Name of election', default='')
+    
+    # Disallow random tiebreaks, ending the election instead
+    parser.add_argument('-r','--disallow-random-tiebreak', help='Halt election instead of using random tiebreak', action='store_true')
+    
+    # Verbose printing of election results
+    parser.add_argument('-v', '--verbose', help='Verbose printing of election results', action='store_true')
+
+    args = parser.parse_args()
+    return args
+
+def process_args(args):
+    """Processes command-line election arguments and runs election.
+
+    Args:
+        argparse.Namespace containing election arguments.
+    """
     if args.ballots != None:
         ballots = ballots_from_file(args.ballots)
     else:
@@ -134,29 +234,11 @@ def main(args):
     if args.verbose:
         pass
 
-    print("Winners:")
+    print('Winners:')
     for candidate in results.candidates_elected:
         print(candidate)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Configure and run an election.')
-    required_group = parser.add_argument_group('required arguments')
-    # Number of seats
-    required_group.add_argument('-s','--seats', help='Number of seats', type=int, required=True)
-
-    # Alphanumeric string for breaking ties
-    parser.add_argument('-a','--alphanumeric', help='Alphanumeric string for breaking ties')
-    # File containing ballots
-    parser.add_argument('-b','--ballots', help='File containing ballots')
-    # Disallow No Confidence from being eliminated
-    parser.add_argument('-c','--disallow-nc-elimination', help='No Confidence cannot be eliminated', action='store_true')
-    # Name of election
-    parser.add_argument('-n','--name', help='Name of election', default='')
-    # Disallow random tiebreaks, ending the election instead
-    parser.add_argument('-r','--disallow-random-tiebreak', help='Halt election instead of using random tiebreak', action='store_true')
-    # Verbose printing of election results
-    parser.add_argument('-v', '--verbose', help='Verbose printing of election results', action='store_true')
-
-    args = parser.parse_args()
-    main(args)
+if __name__ == '__main__':
+    election_args = parse_args()
+    process_args(election_args)
